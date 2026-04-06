3aaa0c95d74bb1e35c3cfc467618fc77dac6de38
Ramon Albini <166454486+Ramon-Albini@users.noreply.github.com>
2026-04-06 19:09:36 +0000

    feat: adicionando switch case para o CRUD

--- projetoCRUDlivraria/projeto_crud_livraria/lib/main.dart
+++ projetoCRUDlivraria/projeto_crud_livraria/lib/main.dart
@@ -1,44 +1,46 @@
+import 'dart:io';
+
 
 List<Livro> livros = [];
 
 class Livro{
-    String titulo;
-    String autor;
-    String anoDePublicacao;
-    String id;
+    String _titulo;
+    String _autor;
+    String _anoDePublicacao;
+    String _id;
 
-    Livro(this.titulo, this.autor, this.anoDePublicacao, this.id);
+    Livro(this._titulo, this._autor, this._anoDePublicacao, this._id);
 
-    String getTitulo() {
-        return this.titulo;
+    String? getTitulo() {
+        return this._titulo;
     }
 
     void setTitulo(String novoTitulo) {
-        this.titulo = novoTitulo;
+        this._titulo = novoTitulo;
     }
 
-    String getAutor() {
-        return this.autor;
+    String? getAutor() {
+        return this._autor;
     }
 
     void setAutor(String novoAutor) {
-        this.autor = novoAutor;
+        this._autor = novoAutor;
     }
 
-    String getAnoDePuplicacao() {
-        return this.anoDePublicacao;
+    String? getAnoDePuplicacao() {
+        return this._anoDePublicacao;
     }
 
     void setAnoDePublicacao(String novoAno) {
-        this.anoDePublicacao = novoAno;
+        this._anoDePublicacao = novoAno;
     }
 
-    String getId() {
-        return this.id;
+    String? getId() {
+        return this._id;
     }
 
     void setId(String novoId) {
-        this.id = novoId;
+        this._id = novoId;
     }
 }
 
@@ -49,7 +51,7 @@ void cadastrarLivro(String titulo, String autor, String anoDePublicacao, String 
 
 Livro? getLivro(String nomeLivro) {
     try{
-        Livro livro = livros.firstWhere((e) => e.getTitulo().toLowerCase() == nomeLivro.toLowerCase());
+        Livro? livro = livros.firstWhere((e) => e.getTitulo()?.toLowerCase() == nomeLivro.toLowerCase());
         return livro;
     } catch (e) {
         print("Livro não encontrado.");
@@ -69,13 +71,67 @@ void listarLivros(){
     }
 }
 
+void listarLivroUnico(String nomeLivro) {
+    Livro? livro = getLivro(nomeLivro);
+    print("Titulo: ${livro?.getTitulo()}");
+    print("Autor: ${livro?.getAutor()}");
+    print("Ano de publicação: ${livro?.getAnoDePuplicacao()}");
+    print("Identificação ISBN: ${livro?.getId()}");
+    print("="*50);
+    print("\n");
+}
+
 
 void main() {
 
-    cadastrarLivro("Elantris", "Brian Sanderson", "2005", "0765311771");
-    cadastrarLivro("O Hobbit", "J. R. R. Tolkien", "1937", "000711835-X");
+    cadastrarLivro("Duna", "Frank Herbert", "1965", "978-85-7657-101-8");
+    cadastrarLivro("O Senhor dos Aneis: A sociedade do Anel", "J. R. R. Tolkien", "1954", "978-972-1-04102-8");
     cadastrarLivro("Fundação", "Isaac Asimov", "1951", "9788576574835");
-    //listarLivros();
-    Livro? livroBuscado = getLivro("elantris");
-    print("hello world");
+
+    int? escolha = 0;
+    while (escolha != 5) {
+  
+        print("1 - Cadastrar livro");
+        print("2 - Listar livros");
+        print("3 - Atualizar livro");
+        print("4 - Remover livro");
+        print("5 - Sair");
+        stdout.write("Escolha o n° de uma opção: ");
+        String? entrada = stdin.readLineSync();
+        if(entrada == null || entrada.trim().isEmpty) { 
+            stderr.writeln("Escolha entre 1-5!!");
+            continue;
+        }
+        try{
+            escolha = int.parse(entrada);
+            if(escolha <= 0 || escolha > 5) {
+                stderr.writeln("Escolha entre 1-5!!");
+            }
+
+        } catch(e) {
+            stderr.writeln("Escolha entre 1-5!!");
+        }
+
+        switch (escolha) {
+            case 1:
+                print("Cadastrar livro");
+                stdout.write("Digite o título do livro: ");
+                String? entradaNome = stdin.readLineSync();
+                
+                stdout.write("Digite o autor do livro: ");
+                String? entradaAutor = stdin.readLineSync();
+                
+                stdout.write("Digite o ano de publicação do livro: ");
+                String? entradaAno = stdin.readLineSync();
+                
+                stdout.write("Digite o ISBN do livro: ");
+                String? entradaId = stdin.readLineSync();
+
+                cadastrarLivro(entradaAno)
+                
+                break;
+        }
+    }
+    listarLivroUnico("elantris");
+
 }
